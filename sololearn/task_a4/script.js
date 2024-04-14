de6ac5788a8683cc0f33ac7ded74b6d67b8bec32
Ezh1337 <illiaq20@gmail.com>
2024-04-14 10:37:16 +0200

    fix

--- sololearn/task_a4/script.js
+++ sololearn/task_a4/script.js
@@ -18,20 +18,49 @@ function regn(){
         return
     }
 }  
+var displayArray = function(array) {
+    framvisning.innerHTML = array;
+    
+};
 var swap = function(array, firstIndex, secondIndex) {
-    let temp = array[firstIndex]
-	array[firstIndex] = array[secondIndex];
-	array[secondIndex] = temp;
+    var temp = array[firstIndex];
+    array[firstIndex] = array[secondIndex];
+    array[secondIndex] = temp;
 };
 
-var testArray = [7, 9, 4];
-swap(testArray, 1, 0);
+var indexOfMinimum = function(array, startIndex) {
+    var minValue = array[startIndex];
+    var minIndex = startIndex;
 
+    for(var i = startIndex + 1; i < array.length; i++) {
+        if(array[i] < minValue) {
+            minIndex = i;
+            minValue = array[i];
+        }
+    } 
+    return minIndex;
+};
+var selectionSort = function(array) {
+for(var i = 0; i < array.length; i++) {
+        
+        var minIndex = indexOfMinimum(array, i);
+       
+        swap(array, i, minIndex);
+    }
+};
 
+var array = [2, 1];
+selectionSort(array);
+var array1 = [22, 11, 99, 88, 9, 7, 42];
+selectionSort(array1);
+var array2 =[-1,-5,8,9,0,77];
+selectionSort(array2);
+displayArray(array);
+displayArray(array2);
+displayArray(array1);
+console.log("Array after sorting:  " + array.join(", "));
+console.log("Array after sorting:  " + array2.join(", "));
+console.log("Array after sorting:  " + array1.join(", "));
 
-
-console.log(testArray);
-
-//Program.assertEqual(testArray, [9, 7, 4]);
 
 
